ed78e16fc502b6e412d8da4a360dcb0b53ec630c
rpulkka <robert.pulkka@gmail.com>
2022-03-09 16:15:08 +0200

    Exercise 10.12: Environment Variables

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,7 +1,8 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import Constants from 'expo-constants'
 
 const httpLink = createHttpLink({
-  uri: 'http://10.112.27.244:4000/graphql',
+  uri: Constants.manifest.extra.apolloURI,
 })
 
 const createApolloClient = () => {
